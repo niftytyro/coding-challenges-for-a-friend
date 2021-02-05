3d41d2fdabdd2e6607f2f3a4fc43b7048a067000
Udasi Tharani <udasitharani@gmail.com>
2021-02-05 20:05:21 +0530

    adding problem statement

--- parenthesis_matcher.py
+++ parenthesis_matcher.py
@@ -1,3 +1,39 @@
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+# An input string is valid if:
+
+# Open brackets must be closed by the same type of brackets. Open brackets must be closed in the correct order.
+
+# Input Format
+
+# First line of input contains string s
+
+# Constraints
+
+# 1 <= s.length <= 104
+# s consists of parentheses only '()[]{}'.
+# Output Format
+
+# Print 1 if valid else print 0.
+
+# Sample Input 0
+
+# ()
+# Sample Output 0
+
+# 1
+# Sample Input 1
+
+# ()[]{}
+# Sample Output 1
+
+# 1
+# Sample Input 2
+
+# (]
+# Sample Output 2
+
+# 0
 opening_braces = ["(", "[", "{"]
 closing_braces = [")", "]", "}"]
 braces_order = []
